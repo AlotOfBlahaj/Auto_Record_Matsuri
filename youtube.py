44f649160f838e0c7be2a44587a9db7385b0a4f4
fzxiao233 <fzxiao@Outlook.com>
2019-02-18 23:58:05 +0800

    现在将视频下载改为在直播结束后

--- youtube.py
+++ youtube.py
@@ -5,15 +5,18 @@ import time
 from config import *
 from tools import gethtml
 
+is_live = False
 
-class Youtube(object):
+
+class Youtube:
+
     def __init__(self):
         self.ChannelID = ChannelID
 
     # 关于SearchAPI的文档 https://developers.google.com/youtube/v3/docs/search/list
     def get_videoid_by_channelid(self):
         channel_info = json.loads(gethtml(r'https://www.googleapis.com/youtube/v3/search?key={}&channelId={}'
-                                          r'&part=snippet,id&order=date&maxResults=3'.format(ApiKey, self.ChannelID)))
+                                          r'&part=snippet,id&order=date&maxResults=5'.format(ApiKey, self.ChannelID)))
         # 判断获取的数据是否正确
         if channel_info['items']:
             vid = []
@@ -42,22 +45,30 @@ class Youtube(object):
                       '{} is not a live video'.format(info_dict['Title']))
 
     def judge(self):
+        global is_live
+        if not is_live:
             if 'LIVE NOW' in self.html:
-                vid = self.getlive_vid()
-                if vid:
-                    self.downloader(r"https://www.youtube.com/watch?v=" + vid)
+                is_live = self.getlive_vid()
+                print('Youtube' + time.strftime('|%m-%d %H:%M:%S|', time.localtime(time.time())) +
+                      'Found A Live, waiting for it to close'.format(sec))
             elif 'Upcoming live streams' in self.html:
                 print('Youtube' + time.strftime('|%m-%d %H:%M:%S|', time.localtime(time.time())) +
                       'Found A Live Upcoming, after {}s checking'.format(sec))
             else:
                 print('Youtube' + time.strftime('|%m-%d %H:%M:%S|', time.localtime(time.time())) +
                       'Not found Live, after {}s checking'.format(sec))
+        else:
+            if 'LIVE NOW' not in self.html:
+                self.downloader(r"https://www.youtube.com/watch?v=" + is_live)
+            else:
+                print('Youtube' + time.strftime('|%m-%d %H:%M:%S|', time.localtime(time.time())) +
+                      'Found A Live, waiting for it to close'.format(sec))
 
     def downloader(self,link):
         while True:
             os.system(r"youtube-dl --proxy http://{} -o {}/%(title)s.%(ext)s {}".format(proxy, ddir, link))
             print('Youtube' + 'Download is broken. Retring \n If the notice always happend, '
-                               'please delete the dir ".part" file')
+                              'please delete the dir ".part" file')
             if '.part' not in os.listdir(ddir):
                 break
 
