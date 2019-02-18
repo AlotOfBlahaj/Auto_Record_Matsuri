5b45fd52783d528f14f3abb61926d5e61edb9446
fzxiao233 <fzxiao@Outlook.com>
2019-02-18 23:58:05 +0800

    现在将视频下载改为在直播结束后

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import asyncio
 import time
 
-from config import sec, sec_error
+from config import sec, sec_error, enable_mirrativ, enable_youtube
 from mirrativ import Mirrativ
 from youtube import Youtube
 
 
-class Localtimer(object):
+class Localtimer:
 
     async def youtube_timer(self):
         y = Youtube()
@@ -24,11 +24,35 @@ async def main():
     tasks = asyncio.gather(t.youtube_timer(), t.mirrativ_timer())
     await tasks
 
-if __name__ == '__main__':
-    while True:
-        try:
-            asyncio.run(main())
-        except:
-            print('Something wrong. After {}s retrying'.format(sec_error))
-            time.sleep(sec_error)
 
+def error():
+    print('Something wrong. After {}s retrying'.format(sec_error))
+    time.sleep(sec_error)
+
+
+if __name__ == '__main__':
+    if enable_mirrativ and enable_youtube:
+        while True:
+            try:
+                asyncio.run(main())
+            except:
+                error()
+    elif enable_youtube and enable_mirrativ == 0:
+        while True:
+            try:
+                y = Youtube()
+                y.check()
+                time.sleep(sec)
+            except:
+                error()
+    elif enable_mirrativ and enable_youtube == 0:
+        while True:
+            try:
+                m = Mirrativ()
+                m.check()
+                time.sleep(sec)
+            except:
+                error()
+    else:
+        print('You should enable a module')
+        exit(-1)
