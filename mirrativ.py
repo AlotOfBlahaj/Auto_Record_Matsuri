7fd0aaf4b31516b913a200ff06ff7f440dada2f4
fzxiao233 <fzxiao@Outlook.com>
2019-02-22 11:30:39 +0800

    修正代理设置

--- mirrativ.py
+++ mirrativ.py
@@ -10,20 +10,24 @@ class Mirrativ:
     def __init__(self, userid, enable_proxy, proxy, ddir):
         self.ddir = ddir
         self.id = userid
+        self.enable_proxy = enable_proxy
         if enable_proxy == 1:
-            self.proxy = '--https-proxy ' + f'"http://{proxy}"'
+            self.dl_proxy = '--https-proxy ' + f'"http://{proxy}"'
+            self.proxy = proxy
         else:
             self.proxy = ''
 
     def live_info(self):
-        l_info = json.loads(gethtml(f'https://www.mirrativ.com/api/user/profile?user_id={self.id}'))
+        l_info = json.loads(gethtml(f'https://www.mirrativ.com/api/user/profile?user_id={self.id}',
+                                    self.enable_proxy, self.proxy))
         nowlive = l_info['onlive']
         if nowlive:
             return nowlive['live_id']
 
     def get_hsl(self):
         live_id = self.live_info()
-        hsl_info = json.loads(gethtml(f'https://www.mirrativ.com/api/live/live?live_id={live_id}'))
+        hsl_info = json.loads(gethtml(f'https://www.mirrativ.com/api/live/live?live_id={live_id}',
+                                      self.enable_proxy, self.proxy))
         title = hsl_info['shares']['twitter']['card']['title']
         steaming_url = hsl_info['streaming_url_hls']
         self.downloader(steaming_url, title)
