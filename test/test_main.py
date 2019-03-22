f3c5bd34ab48a0ee15c9fc653af83883e0d01c06
fzxiao233 <fzxiao@Outlook.com>
2019-03-22 21:38:44 +0800

    修改测试脚本

--- test/test_main.py
+++ test/test_main.py
@@ -3,23 +3,33 @@ import sys
 import pytest
 
 from mirrativ import Mirrativ
+from openrec import Openrec
 from youtube import Youtube
 
 sys.path.insert(0, '..')
 
 
 def test_mirrativ():
-    userid = '3264432'
-    m = Mirrativ(userid)
-    m.check()
+    userid = ['3264432']
+    m = Mirrativ()
+    for x in userid:
+        m.check(x)
 
 
 def test_youtube():
     apikey = ''
-    channel_id = 'UCQ0UDLQCjY0rmuxCDE38FGg'
+    channel_id = ['UCQ0UDLQCjY0rmuxCDE38FGg']
     quality = '720p'
-    y = Youtube(channel_id, apikey, quality)
-    y.check()
+    y = Youtube(apikey, quality)
+    for x in channel_id:
+        y.check(x)
+
+
+def test_openrec():
+    oprec_id = ['natsuiromatsuri']
+    o = Openrec()
+    for x in oprec_id:
+        o.check(x)
 
 
 if __name__ == '__main__':
