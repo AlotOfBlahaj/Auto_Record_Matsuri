0dcd05b0639ccdc8b4dbab0f12080c81ca9fe204
fzxiao233 <fzxiao@Outlook.com>
2019-03-23 20:45:27 +0800

    增加在线链接显示 增加添加网盘链接到数据库

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ enable_proxy = 0
 proxy = '127.0.0.1:10800'
 # 保存位置
 ddir = '/root/Matsuri'  # 这里后边不应该有斜杠
-# YoutubeAPI3Key 申请地址：https://console.developers.google.com/apis/library/youtube.googleapis.com?q=Youtube&id=125bab65-cfb6-4f25-9826-4dcc309bc508&project=youtube-streaming-231512
+# YoutubeAPI3Key 申请地址：https://console.developers.google.com/apis/library/youtube.googleapis.com
 api_key = ''
 # 监测频道ID 可填入多个频道，格式为['第一个地址', '第二个地址']
 channel_id = ['UCQ0UDLQCjY0rmuxCDE38FGg']  # UCQ0UDLQCjY0rmuxCDE38FGg
