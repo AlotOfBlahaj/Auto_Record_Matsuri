0bdc5c368056b99cb6e47157d0d8f85bc9e973b0
fzxiao233 <fzxiao@Outlook.com>
2019-03-31 10:25:30 +0800

    修复Data key错误的问题

--- twitcasting.py
+++ twitcasting.py
@@ -1,4 +1,3 @@
-import asyncio
 import json
 import time
 
@@ -38,7 +37,3 @@ class Twitcasting:
         else:
             echo_log('Twitcasting' + time.strftime('|%m-%d %H:%M:%S|', time.localtime(time.time())) +
                      f'Not found Live, after {sec}s checking')
-
-
-t = Twitcasting()
-asyncio.run(t.get_hsl('natsuiromatsuri'))
