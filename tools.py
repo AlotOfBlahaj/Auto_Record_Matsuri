508cf1b713eae3410e3fb06ef28548e876afb73d
fzxiao233 <fzxiao@Outlook.com>
2019-04-05 21:31:41 +0800

    增加在线链接显示 增加添加网盘链接到数据库

--- tools.py
+++ tools.py
@@ -177,8 +177,8 @@ async def process_video(is_live, model):
     database = Database()
     database.insert(is_live['Title'], 'https://pan.baidu.com/s/' + link, is_live['Date'])
     get_logger(share)
-    await bot(f"[下载提示] {is_live['Title']} 已上传" + share)
-
+    # await bot(f"[下载提示] {is_live['Title']} 已上传" + share)
+    await bot(f"[下载提示] {is_live['Title']} 已上传, 请查看页面")
 
 class Database:
     def __init__(self):
