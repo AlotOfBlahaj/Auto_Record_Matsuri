e37bb120b0b972b54107e1fceaf3736da0b819a4
fzxiao233 <fzxiao@outlook.com>
2019-04-20 22:32:39 +0800

    修正偶发的empty错误
    Signed-off-by: fzxiao233 <fzxiao@Outlook.com>

--- main.py
+++ main.py
@@ -14,51 +14,59 @@ from video_process import inner
 from youtube import Youtube
 
 
-def create_task_list():
-    y = Youtube(api_key, quality)
-    t = Twitcasting()
-    o = Openrec()
-    m = Mirrativ()
-    b = Bilibili()
-    task = []
-    if enable_youtube:
-        while youtube_queue.qsize():
-            task.append(asyncio.create_task(y.check(youtube_queue.get_nowait())))
-    if enable_twitcasting:
-        while twitcasting_queue.qsize():
-            task.append(asyncio.create_task(t.check(twitcasting_queue.get_nowait())))
-    if enable_openrec:
-        while openrec_queue.qsize():
-            task.append(asyncio.create_task(o.check(openrec_queue.get_nowait())))
-    if enable_mirrativ:
-        while mirrativ_queue.qsize():
-            task.append(asyncio.create_task(m.check(mirrativ_queue.get_nowait())))
-    if enable_bilibili:
-        for x in bilibili_id:
-            task.append(asyncio.create_task(b.check(x)))
-    return task
+class Main:
+    def __init__(self):
+        if enable_youtube:
+            self.y = Youtube(api_key, quality)
+        if enable_twitcasting:
+            self.t = Twitcasting()
+        if enable_openrec:
+            self.o = Openrec()
+        if enable_mirrativ:
+            self.m = Mirrativ()
+        if enable_bilibili:
+            self.b = Bilibili()
 
+    def create_task_list(self):
+        task = []
+        if enable_youtube:
+            while youtube_queue.qsize():
+                task.append(asyncio.create_task(self.y.check(youtube_queue.get_nowait())))
+        if enable_twitcasting:
+            while twitcasting_queue.qsize():
+                task.append(asyncio.create_task(self.t.check(twitcasting_queue.get_nowait())))
+        if enable_openrec:
+            while openrec_queue.qsize():
+                task.append(asyncio.create_task(self.o.check(openrec_queue.get_nowait())))
+        if enable_mirrativ:
+            while mirrativ_queue.qsize():
+                task.append(asyncio.create_task(self.m.check(mirrativ_queue.get_nowait())))
+        if enable_bilibili:
+            for x in bilibili_id:
+                task.append(asyncio.create_task(self.b.check(x)))
+        return task
 
-async def main():
-    queue_init()
-    while True:
-        tasks = create_task_list()
-        result = [await run for run in tasks]
-        for data in result:
-            if data:
-                video_info = data[0]
-                module = data[1]
-                p = multiprocessing.Process(target=proc, args=(video_info, module))
-                p.start()
-        sleep(sec)
+    async def main(self):
+        queue_init()
+        sleep(0.5)
+        while True:
+            tasks = self.create_task_list()
+            result = [await run for run in tasks]
+            for data in result:
+                if data:
+                    video_info = data[0]
+                    module = data[1]
+                    p = multiprocessing.Process(target=self.proc, args=(video_info, module))
+                    p.start()
+            sleep(sec)
 
-
-def proc(video_info, call_back):
-    inner(video_info, call_back)
-    queue = queue_map(call_back['Module'])
-    if call_back['Target']:
-        add_queue(queue, call_back['Target'])
+    def proc(self, video_info, call_back):
+        inner(video_info, call_back)
+        queue = queue_map(call_back['Module'])
+        if call_back['Target']:
+            add_queue(queue, call_back['Target'])
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    MAIN = Main()
+    asyncio.run(MAIN.main())
