ce7019533ff2d02198c907c2452b5ef07d6d27fd
fzxiao <fzxiao@Outlook.com>
2019-05-11 22:04:50 +0800

    Twitcasting重构部分代码
    Signed-off-by: fzxiao <fzxiao@Outlook.com>

--- twitcasting.py
+++ twitcasting.py
@@ -1,8 +1,6 @@
 import time
 from multiprocessing import Process
 
-from lxml.html import etree
-
 from config import sec
 from daemon import VideoDaemon
 from queues import twitcasting_queue
@@ -13,6 +11,7 @@ class Twitcasting(VideoDaemon):
     def __init__(self):
         super().__init__(twitcasting_queue)
         self.logger = get_logger('Twitcasting')
+        self.module = 'Twitcasting'
 
     @staticmethod
     def live_info(twitcasting_id):
@@ -27,9 +26,9 @@ class Twitcasting(VideoDaemon):
     @staticmethod
     def get_hsl(twitcasting_id, live_info):
         html = get(f"https://twitcasting.tv/{twitcasting_id}")
-        dom = etree.HTML(html)
-        title = dom.xpath('/html/body/div[3]/div[2]/div/div[2]/h2/span[3]/a/text()')[0]
-        title += '|' + live_info.get('Vid')
+        # dom = etree.HTML(html)
+        # title = dom.xpath('/html/body/div[3]/div[2]/div/div[2]/h2/span[3]/a/text()')[0]
+        title = twitcasting_id + live_info.get('Vid')
         ref = f"https://twitcasting.tv/{twitcasting_id}/metastream.m3u8"
         target = f"https://twitcasting.tv/{twitcasting_id}"
         date = time.strftime("%Y-%m-%d", time.localtime())
@@ -42,10 +41,10 @@ class Twitcasting(VideoDaemon):
         live_info = self.live_info(twitcasting_id)
         if live_info.get('Is_live'):
             result = self.get_hsl(twitcasting_id, live_info)
-            self.put_download([result, {'Module': 'Twitcasting', 'Target': twitcasting_id}])
+            self.put_download([result, {'Module': 'Twitcasting', 'Target': self.module}])
         else:
             self.logger.info(f'Not found Live, after {sec}s checking')
-            self.return_and_sleep(twitcasting_id, 'Twitcasting')
+            self.return_and_sleep(twitcasting_id, self.module)
 
     def actor(self, twitcasting_id):
         proc = Process(target=self.check, args=(twitcasting_id,))
