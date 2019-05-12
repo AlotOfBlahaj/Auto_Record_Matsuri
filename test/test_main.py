91cdbcb2dd2c7854b0fd585fb5a4cbc2a88be206
fzxiao <fzxiao@Outlook.com>
2019-05-12 11:41:13 +0800

    重构部分代码
    Signed-off-by: fzxiao <fzxiao@Outlook.com>

--- test/test_main.py
+++ test/test_main.py
@@ -2,20 +2,27 @@ import sys
 
 import pytest
 
-from video_process import title_block
+from video_process import AdjustFileName, process_video
 
 sys.path.insert(0, '..')
 
 
-def test_title_block():
+def test_adjust_title():
     replace_list = ['|', '/', '\\']
-    title = '夏まつch|542038802.ts'
-    result = title_block(title)
+    title = '夏まつch|542038802'
+    result = AdjustFileName(title).adjust()
     print(result)
     for x in replace_list:
         assert x not in result
 
 
+def test_process_video():
+    live_dict = {'Title': '【歌ってみた】スイートマジック 【夏色まつり×紫咲シオン】',
+                 'Ref': '97lcMrPqzHg',
+                 'Date': '2019-05-12',
+                 'Target': 'https://www.youtube.com/watch?v=97lcMrPqzHg'}
+    call_back = {'Module': 'Youtube', 'Target': 'UCQ0UDLQCjY0rmuxCDE38FGg'}
+    process_video(live_dict, call_back)
 
 
 if __name__ == '__main__':
--- video_process.py
+++ video_process.py
@@ -2,7 +2,7 @@ import re
 import subprocess
 from os import name
 from os.path import isfile
-from time import strftime, localtime, time
+from time import sleep
 
 from config import enable_upload, ddir, enable_proxy, proxy, enable_db
 from queues_process import queue_map, Queue
@@ -28,27 +28,26 @@ def bd_upload(file):
         s2 = subprocess.run(command2, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                             encoding='utf-8', universal_newlines=True)
         share_info = s2.stdout
-        if 's' not in share_info:
-            return None
-        while True:
-            if 'https' in share_info:
-                share_info = share_info.replace('\n', '')
-                logger.info('Share success')
-                break
-            else:
-                share_info = s2.stdout
-                logger.error('Share failed')
+        if 'https' in share_info:
+            share_info = share_info.replace('\n', '')
+            logger.info(f'{file}: Share successful {share_info}')
+        else:
+            logger.error('Share failed')
+            raise RuntimeError(f'{file} share failed')
         reg = r'https://pan.baidu.com/s/([A-Za-z0-9_-]{23})'
         linkre = re.compile(reg)
         link = re.search(linkre, share_info)
         try:
             link = 'https://pan.baidu.com/s/' + link.group(1)
+            return link
         except AttributeError:
-            raise RuntimeError('Error')
-        return link
+            logger.exception('get share link error')
+            raise RuntimeError('get share link error')
+    return None
 
 
-def downloader(link, title, dl_proxy):
+def downloader(link, title, dl_proxy, quality='best'):
+    logger = get_logger('Downloader')
     # co = ["streamlink", "--hls-live-restart", "--loglevel", "trace", "--force"]
     co = ["streamlink", "--hls-live-restart", "--force"]
     if enable_proxy:
@@ -59,72 +58,76 @@ def downloader(link, title, dl_proxy):
     co.append("-o")
     co.append(f"{ddir}/{title}")
     co.append(link)
-    co.append("best")
+    co.append(quality)
     subprocess.run(co)
+    paths = f'{ddir}/{title}'
+    if isfile(paths):
+        logger.info(f'{title} has been downloaded.')
+        bot(f"[下载提示] {title} 已下载完成，等待上传")
+    else:
+        logger.error(f'{title} Download error, link: {link}')
+        raise RuntimeError(f'{title} Download error, link: {link}')
     # 不应该使用os.system
 
 
-def title_block(filename):
-    replace_list = ['|', '/', '\\']
-    for x in replace_list:
-        filename = filename.replace(x, '#')
-    return filename
+class AdjustFileName:
 
+    def __init__(self, filename):
+        self.filename = filename
 
-def file_exist(filename: str) -> str:
-    paths = f'{ddir}/{filename}.ts'
-    if isfile(paths):
-        n = 0
-        while True:
-            new_filename = filename + f'_{n}.ts'
-            if not isfile(f'{ddir}/{new_filename}'):
-                return new_filename
-            n += 1
-    else:
-        return filename + '.ts'
+    def title_block(self):
+        replace_list = ['|', '/', '\\']
+        for x in replace_list:
+            self.filename = self.filename.replace(x, '#')
 
+    def file_exist(self):
+        paths = f'{ddir}/{self.filename}.ts'
+        if isfile(paths):
+            n = 0
+            while True:
+                new_filename = self.filename + f'_{n}.ts'
+                if not isfile(f'{ddir}/{new_filename}'):
+                    self.filename = new_filename
+                    break
+                n += 1
+        else:
+            self.filename = self.filename + '.ts'
 
-def filename_length_limit(filename: str) -> str:
-    lens = len(filename)
-    if lens > 80:
-        return filename[:80]
-    return filename
+    def filename_length_limit(self):
+        lens = len(self.filename)
+        if lens > 80:
+            self.filename = self.filename[:80]
+
+    def adjust(self):
+        self.title_block()
+        self.filename_length_limit()
+        self.file_exist()
+        return self.filename
+
 
 def process_video(data, call_back):
     bot(f"[直播提示] {call_back['Module']}{data.get('Title')} 正在直播 链接: {data['Target']}")
 
     logger = get_logger('Process Video')
-    logger.info(call_back['Module'] + strftime('|%m-%d %H:%M:%S|', localtime(time())) +
-                'Found A Live, starting downloader')
+    logger.info(f'{call_back["Module"]} Found A Live, starting downloader')
 
-    data['Title'] = title_block(data['Title'])
-    # issue #61
-    data['Title'] = filename_length_limit(data['Title'])
-    # issue #37
-    data['Title'] = file_exist(data['Title'])
-
-    if call_back['Module'] == 'Youtube':
-        downloader(r"https://www.youtube.com/watch?v=" + data['Ref'], data['Title'], proxy)
-    else:
-        downloader(data['Ref'], data['Title'], proxy)
-
-    logger.info(call_back['Module'] + strftime("|%m-%d %H:%M:%S|", localtime(time())) +
-                f"{data['Title']} was already downloaded")
-
-    bot(f"[下载提示] {data['Title']} 已下载完成，等待上传")
-
-    link = bd_upload(f"{data['Title']}")
-    if enable_db:
-        database = Database('Video')
-    if not call_back['Module'] == 'Mirrativ':
+    data['Title'] = AdjustFileName(data['Title']).adjust()
+    try:
+        if call_back['Module'] == 'Youtube':
+            downloader(r"https://www.youtube.com/watch?v=" + data['Ref'], data['Title'], proxy, '720p')
+        else:
+            downloader(data['Ref'], data['Title'], proxy)
+        link = bd_upload(f"{data['Title']}")
         if link:
             if enable_db:
-                database.insert(data['Title'], link, data['Date'])
-        bot(f"[下载提示] {data['Title']} 已上传, 请查看页面")
-    else:
-        bot(f"[下载提示] {data['Title']} 已上传" + link)
-
-    return_queue(call_back)
+                db = Database('Video')
+                db.insert(data['Title'], link, data['Date'])
+            bot(f"[下载提示] {data['Title']} 已上传, 请查看页面")
+    except RuntimeError:
+        return None
+    finally:
+        sleep(1)
+        return_queue(call_back)
 
 
 def return_queue(call_back):
