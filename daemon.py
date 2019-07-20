86c1cb380ae816a7adb59a6e21b9b6e50cd369e9
fzxiao233 <fzxiao@outlook.com>
2019-07-20 16:29:24 +0800

    修改上传的实现方式
    Signed-off-by: fzxiao233 <fzxiao@outlook.com>

--- daemon.py
+++ daemon.py
@@ -1,17 +1,15 @@
 from abc import ABCMeta, abstractmethod
 from multiprocessing import Process
 
-from queues import upload_queue
 from tools import get_logger
 from upload import upload_video
 
 
 class VideoUpload(Process):
-    def __init__(self):
+    def __init__(self, video_dict):
         super().__init__()
-        self.queue = upload_queue
         self.logger = get_logger('VideoUpload')
-        self.video_info = None
+        self.video_dict = video_dict
 
     def run(self) -> None:
         try:
@@ -20,11 +18,7 @@ class VideoUpload(Process):
             exit(0)
 
     def start_daemon(self):
-        self.logger.info('Waiting for tasks')
-        while True:
-            video_info = self.queue.get()
-            self.video_info = video_info
-            upload_video(self.video_info)
+        upload_video(self.video_dict)
 
 
 class VideoDaemon(Process, metaclass=ABCMeta):
